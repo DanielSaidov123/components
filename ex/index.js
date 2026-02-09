95bb00d14d6964dd6071c5ecdc7c3ca415891fe6
DanielSaidov123 <ddny3622@gmail.com>
2026-02-09 17:55:05 +0200

    aadd

--- ex/index.js
+++ ex/index.js
@@ -1,8 +1,18 @@
-const main = document.getElementById("main");
-let originalMain = null;
-const arr = [];
-let root;
+const fader = document.getElementById("fader");
+const continer2 = document.getElementById("continer");
+
+let root1;
+let root2;
+
 function d(title, h1, p, imgg, id) {
+  const data = {
+    userId: title,
+    title: h1,
+    completed: p,
+    img: imgg,
+    id,
+  };
+
   const container = document.createElement("div");
   container.classList = "continer";
   container.id = id;
@@ -22,10 +32,11 @@ function d(title, h1, p, imgg, id) {
   divp.textContent = p;
   divp.classList = "p";
   container.append(divp);
-  root = container;
-  arr.push(container);
-  main.append(root);
-  attachOpenBehavior(container);
+  container.addEventListener("click", () => {
+    if (root1.id === "home") return replaceToPostPage(data);
+    replaceToHome();
+  });
+  return container;
 }
 
 async function fetchDataWrapper(num) {
@@ -38,7 +49,6 @@ async function fetchDataWrapper(num) {
     }
 
     const data = await res.json();
-    console.log(data);
 
     return data;
   } catch (error) {
@@ -47,33 +57,87 @@ async function fetchDataWrapper(num) {
 }
 
 async function createData() {
+  const main = document.createElement("main");
+  main.id = "home";
   for (let i = 1; i < 5; i++) {
     const data1 = await fetchDataWrapper(i);
     if (data1)
-      d(
-        data1.userId,
-        data1.title,
-        data1.completed,
-        `https://picsum.photos/400/400?random=${i}`,
-        `id${i}`,
+      main.append(
+        d(
+          data1.userId,
+          data1.title,
+          data1.completed,
+          `https://picsum.photos/400/400?random=${i}`,
+          `id${i}`,
+        ),
       );
   }
+  return main;
 }
-function attachOpenBehavior(container) {
-  container.addEventListener("click", () => {
-    if (!originalMain) {
-      originalMain = main.cloneNode(true);
 
-      const bigPost = container.cloneNode(true);
-      bigPost.classList.add("big");
-    
-      main.replaceWith(bigPost);
+async function replaceToHome() {
+  const main = await createData();
+  root1.replaceWith(main);
+  root1 = main;
+}
 
-      bigPost.addEventListener("click", () => {
-        bigPost.replaceWith(originalMain);
-        originalMain = null;
-      });
+async function replaceToPostPage(data) {
+  const postPage = d(
+    data.userId,
+    data.title,
+    data.completed,
+    data.img,
+    data.id,
+  );
+  root1.replaceWith(postPage);
+  root1 = postPage;
+}
+
+function chengeButen() {
+  const btns = document.querySelector(".divnav");
+
+  btns.addEventListener("click", (e) => {
+    if (e.target.tagName !== "BUTTON") return;
+
+    if (e.target.id === "button2") {
+      root1 = continer2.cloneNode(true);
+      continer2.replaceWith(createDynamicForm());
+    }
+
+    if (e.target.id === "button1" && root1) {
+      document.querySelector("form").replaceWith(root1);
     }
   });
 }
-createData();
+
+function createDynamicForm() {
+  const h1 = document.querySelector("h1");
+  h1.textContent = "Creating a news story";
+
+  const form = document.createElement("form");
+  form.method = "POST";
+
+  const inputElement = document.createElement("input");
+  inputElement.type = "text";
+  inputElement.name = "title";
+  inputElement.placeholder = "title";
+
+  const submitButton = document.createElement("input");
+  submitButton.type = "submit";
+  submitButton.value = "Submit";
+
+  form.append(inputElement, submitButton);
+  return form;
+}
+
+// async function replaceToHome() {
+//   const main = await createData();
+//   root1.replaceWith(main);
+//   root1 = main;
+// }
+
+
+
+const main = await createData();
+document.querySelector("body").append(main);
+root1 = main;
